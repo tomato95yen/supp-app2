42f5bcba862f433b31153778cc8d850771bb4f94
tomato95yen <74520325+tomato95yen@users.noreply.github.com>
2021-02-08 17:19:44 +0900

    active hashの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,12 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   belongs_to :user, optional: true
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    validates :prefecture_id
+    validates :prefecture_id, numericality: {other_than:1 }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A\d{,11}\z/ }
